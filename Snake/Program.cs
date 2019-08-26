7d8d4d218a3de820ba5fa9a02138014ffc725fa9
vitalyeirich <vitalyeirich@gmail.com>
2019-08-26 16:50:05 +0700

    Поправил

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -6,7 +6,7 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World");
         }
     }
 }
